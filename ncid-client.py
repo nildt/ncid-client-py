428edac924976aa9cb635c388e00bfae18f66df0
nildt <nilswildt@web.de>
2013-08-17 03:40:31 +0200

    Improved

--- ncid-client.py
+++ ncid-client.py
@@ -2,28 +2,57 @@
 # -*- coding: utf-8 -*-
 #At the moment only testing...#
 '''simple tcp client'''
-import pynotify
-import time
+
+# notification in Ubuntu
+import pynotify 
+import gtk.gdk
+
 # apt-get install python-configobj
 from configobj import ConfigObj
-
 # apt-get install python-twisted
 from twisted.internet.protocol import ClientFactory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
 from datetime import datetime
-
+# Read config file
 CONFIG = ConfigObj('./ncid-client.cfg')
+# Reads config and gives an exemple
 SERVER_IP = CONFIG.get('server_ip', '192.168.2.1')
 SERVER_PORT = int(CONFIG.get('server_port', 3333))
 
+
+class Notify(object):
+	def showNotification(self,notification,zeit):
+		import time
+		pynotify.init("CallAlertWindow")
+		msg = pynotify.Notification("CallAlert", notification, "notification-message-im")
+		msg.set_hint('x', gtk.gdk.screen_width()/2.)
+		msg.set_hint('y', gtk.gdk.screen_height()/2.)
+		msg.set_timeout(zeit)
+		msg.show()
+		
+''' Tried to workaround the close problem... '''
+#		time.sleep(1)
+#		msg.update(" "," "," ")
+#		msg.show()
+#		time.sleep(2)
+#		msg.clsose() 
+
 class NcidClient(LineReceiver):
 	def lineReceived(self, line):
-		if line.startswith('CIDLOG:'):
+		if line.startswith('200 NCID Server:  ARC_ncidd 0.01'):
+			print "Date:	   ","Time: ","Number:"
+		elif line.startswith('CIDLOG:'):
 			data = line.split('*')
 			date, time, phonenr = data[2], data[4], data[8]
 			tstamp_log = datetime.strptime('%s %s' % (date, time), '%d%m%Y %H%M').strftime('%Y-%m-%d %H:%M')
-			print ('%s: %s' % (str(tstamp_log), phonenr))
+			print ('%s %s' % (str(tstamp_log), phonenr))
+		elif line.startswith('CID: '):
+			calldata = line.split('*')
+			phonenr = calldata[8]
+			print ('You are called by ' + '%s' % str(phonenr))
+			notification = Notify()
+			notification.showNotification(('%s ' % str(phonenr)) + "calls you...",5000)
 		else:
 			print line
 
@@ -32,38 +61,29 @@ class NcidClientFactory(ClientFactory):
 
 	def clientConnectionFailed(self, connector, reason):
 		print ('connection failed: %s' % reason.getErrorMessage())
+		notification = Notify()
+		notification.showNotification("Connection failed",1000)
 		reactor.stop()
 
 	def clientConnectionFailed(self, connector, reason):
 		print ('connection lost: %s' % reason.getErrorMessage())
+		notification = Notify()
+		notification.showNotification("Connection lost",1000)
 		reactor.stop()
-		
-class Notify(object):
-	def showNotification(self,notification):
-		pynotify.init("CallAlertWindow2")
-		msg = pynotify.Notification(notification, "Alert", "notification-message-im")
-		msg.set_timeout(10000)
-		msg.show()
 
 def main():
-	print 'initializing...'
-	
-	pynotify.init("CallAlertWindow")
-	msg = pynotify.Notification("Call", "Alert", "notification-message-im")
-	msg.set_timeout(10000)
-	msg.show()
-	time.sleep(1)
-	msg.update("Incoming Call", "Es ruft --- an.", "notification-audio-volume-high")
-	msg.show()
-	
-	test = Notify()
-	test.showNotification("Test")
-	
+	print '[+] Initializing...'
+
+	notification = Notify()
+#	notification.showNotification("Initializing",500) Takes too long to close...
+		
 	factory = NcidClientFactory()
 	reactor.connectTCP(SERVER_IP, SERVER_PORT, factory)
-	print 'started!'
+	print '[+] Started!'
+#	notification.showNotification("Started",500) Takes too long to close...
 	reactor.run()
-	print 'stopped!'
+	print '[+] Stopped!'
+#	notification.showNotification("Stopped",500) Same here :D
 	
 if __name__ == '__main__':
 	main()
