eff9359a666f6e79635e80f045bd621dd2e37b4b
nildt <nilswildt@web.de>
2013-08-18 02:03:32 +0200

    Formatted with PythonTidy-1.22

--- daemon.py
+++ daemon.py
@@ -1,139 +1,170 @@
-"""Generic linux daemon base class for python 3.x."""
-# Uses the daemon implementation from http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/ 
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Uses the daemon implementation from http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
 
-import sys, os, time, atexit, signal
+"""Generic linux daemon base class for python 3.x."""
+
+import sys
+import os
+import time
+import atexit
+import signal
+
 
 class daemon:
-	"""A generic daemon class.
 
-	Usage: subclass the daemon class and override the run() method."""
+    """A generic daemon class.
 
-	def __init__(self, pidfile): self.pidfile = pidfile
-	
-	def daemonize(self):
-		"""Deamonize class. UNIX double fork mechanism."""
+        Usage: subclass the daemon class and override the run() method."""
 
-		try: 
-			pid = os.fork() 
-			if pid > 0:
-				# exit first parent
-				sys.exit(0) 
-		except OSError as err: 
-			sys.stderr.write('fork #1 failed: {0}\n'.format(err))
-			sys.exit(1)
-	
-		# decouple from parent environment
-		os.chdir('/') 
-		os.setsid() 
-		os.umask(0) 
-	
-		# do second fork
-		try: 
-			pid = os.fork() 
-			if pid > 0:
+    def __init__(self, pidfile):
+        self.pidfile = pidfile
 
-				# exit from second parent
-				sys.exit(0) 
-		except OSError as err: 
-			sys.stderr.write('fork #2 failed: {0}\n'.format(err))
-			sys.exit(1) 
-	
-		# redirect standard file descriptors
-		sys.stdout.flush()
-		sys.stderr.flush()
-		si = open(os.devnull, 'r')
-		so = open(os.devnull, 'a+')
-		se = open(os.devnull, 'a+')
+    def daemonize(self):
+        """Deamonize class. UNIX double fork mechanism."""
 
-		os.dup2(si.fileno(), sys.stdin.fileno())
-		os.dup2(so.fileno(), sys.stdout.fileno())
-		os.dup2(se.fileno(), sys.stderr.fileno())
-	
-		# write pidfile
-		atexit.register(self.delpid)
+        try:
+            pid = os.fork()
+            if pid > 0:
 
-		pid = str(os.getpid())
-		with open(self.pidfile,'w+') as f:
-			f.write(pid + '\n')
-	
-	def delpid(self):
-		os.remove(self.pidfile)
+                                # exit first parent
 
-	def start(self):
-		"""Start the daemon."""
-		# Check for a pidfile to see if the daemon already runs
-		try:
-			with open(self.pidfile,'r') as pf:
+                sys.exit(0)
+        except OSError, err:
+            sys.stderr.write('fork #1 failed: {0}\n'.format(err))
+            sys.exit(1)
 
-				pid = int(pf.read().strip())
-		except IOError:
-			pid = None
-	
-		if pid:
-			message = "pidfile {0} already exist. " + \
-					"Daemon already running?\n"
-			sys.stderr.write(message.format(self.pidfile))
-			sys.exit(1)
-		# Start the daemon
-		self.daemonize()
-		self.run()
+                # decouple from parent environment
 
-	def stop(self):
-		"""Stop the daemon."""
+        os.chdir('/')
+        os.setsid()
+        os.umask(0)
 
-		# Get the pid from the pidfile
-		try:
-			with open(self.pidfile,'r') as pf:
-				pid = int(pf.read().strip())
-		except IOError:
-			pid = None
-	
-		if not pid:
-			message = "pidfile {0} does not exist. " + \
-					"Daemon not running?\n"
-			sys.stderr.write(message.format(self.pidfile))
-			return # not an error in a restart
+                # do second fork
 
-		# Try killing the daemon process	
-		try:
-			while 1:
-				os.kill(pid, signal.SIGTERM)
-				time.sleep(0.1)
-		except OSError as err:
-			e = str(err.args)
-			if e.find("No such process") > 0:
-				if os.path.exists(self.pidfile):
-					os.remove(self.pidfile)
-				# Just in case... :D
-				if os.path.isfile('/tmp/CallAlert.pid'):
-					os.remove('/tmp/CallAlert.pid')
-			else:
-#				print (str(err.args))
-				sys.exit(1)
+        try:
+            pid = os.fork()
+            if pid > 0:
 
-	def restart(self):
-		"""Restart the daemon."""
-		self.stop()
-		self.start()
-		
-	def status(self):
-		try:
-			with open(self.pidfile,'r') as pf:
-				pid = int(pf.read().strip())
-		except IOError:
-			pid = None
+                                # exit from second parent
 
-		if not pid:
-			message = "pidfile {0} does not exist. " + \
-				"Daemon not running?\n"
-			sys.stderr.write(message.format(self.pidfile))
-			return # not an error in a restart
+                sys.exit(0)
+        except OSError, err:
+            sys.stderr.write('fork #2 failed: {0}\n'.format(err))
+            sys.exit(1)
 
-		else:
-			print "Running with PID:	" + str(pid)
+                # redirect standard file descriptors
 
-	def run(self):
-		"""You should override this method when you subclass Daemon.
-		
-		It will be called after the process has been daemonized by 
-		start() or restart()."""
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = open(os.devnull, 'r')
+        so = open(os.devnull, 'a+')
+        se = open(os.devnull, 'a+')
+
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+
+                # write pidfile
+
+        atexit.register(self.delpid)
+
+        pid = str(os.getpid())
+        with open(self.pidfile, 'w+') as f:
+            f.write(pid + '\n')
+
+    def delpid(self):
+        os.remove(self.pidfile)
+
+    def start(self):
+        """Start the daemon."""
+
+                # Check for a pidfile to see if the daemon already runs
+
+        try:
+            with open(self.pidfile, 'r') as pf:
+
+                pid = int(pf.read().strip())
+        except IOError:
+            pid = None
+
+        if pid:
+            message = 'pidfile {0} already exist. ' \
+                + 'Daemon already running?\n'
+            sys.stderr.write(message.format(self.pidfile))
+            sys.exit(1)
+
+                # Start the daemon
+
+        self.daemonize()
+        self.run()
+
+    def stop(self):
+        """Stop the daemon."""
+
+                # Get the pid from the pidfile
+
+        try:
+            with open(self.pidfile, 'r') as pf:
+                pid = int(pf.read().strip())
+        except IOError:
+            pid = None
+
+        if not pid:
+            message = 'pidfile {0} does not exist. ' \
+                + 'Daemon not running?\n'
+            sys.stderr.write(message.format(self.pidfile))
+            return   # not an error in a restart
+
+                # Try killing the daemon process
+
+        try:
+            while 1:
+                os.kill(pid, signal.SIGTERM)
+                time.sleep(0.1)
+        except OSError, err:
+            e = str(err.args)
+            if e.find('No such process') > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+
+                                # Just in case... :D
+
+                if os.path.isfile('/tmp/CallAlert.pid'):
+                    os.remove('/tmp/CallAlert.pid')
+            else:
+
+#                               print (str(err.args))
+
+                sys.exit(1)
+
+    def restart(self):
+        """Restart the daemon."""
+
+        self.stop()
+        self.start()
+
+    def status(self):
+        try:
+            with open(self.pidfile, 'r') as pf:
+                pid = int(pf.read().strip())
+        except IOError:
+            pid = None
+
+        if not pid:
+            message = 'pidfile {0} does not exist. ' \
+                + 'Daemon not running?\n'
+            sys.stderr.write(message.format(self.pidfile))
+            return   # not an error in a restart
+        else:
+
+            print 'Running with PID:        ' + str(pid)
+
+    def run(self):
+        """You should override this method when you subclass Daemon.
+                
+                It will be called after the process has been daemonized by 
+                start() or restart()."""
+
+
+
--- ncid-client.py
+++ ncid-client.py
@@ -1,121 +1,147 @@
-#!/usr/bin/python -u
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Uses the daemon implementation from http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
 
-# Uses the daemon implementation from http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/ 
-
+# notification in Ubuntu
 
 '''simple tcp client'''
 
-# notification in Ubuntu
-import pynotify 
+import pynotify
 import gtk.gdk
 
 # I should daemonize it... Want it in Background
+
 from daemon import daemon
 import sys
 import os
 
 # apt-get install python-configobj
+
 from configobj import ConfigObj
 
 # apt-get install python-twisted
+
 from twisted.internet.protocol import ClientFactory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
 from datetime import datetime
 
-# Read config file 
+# Read config file
+
 CONFIG = ConfigObj('./ncid-client.cfg')
+
 # Reads config and gives an exemple
+
 SERVER_IP = CONFIG.get('server_ip', '192.168.2.1')
 SERVER_PORT = int(CONFIG.get('server_port', 3333))
 
-# Prints Notifications
-class Notify(object):
-	def showNotification(self,notification,zeit):
-#		import time
-		pynotify.init("CallAlertWindow")
-		msg = pynotify.Notification("CallAlert", notification, "notification-message-im")
-		msg.set_urgency(pynotify.URGENCY_CRITICAL)
-		msg.set_hint('x', gtk.gdk.screen_width()/2.)
-		msg.set_hint('y', gtk.gdk.screen_height()/2.)
-		msg.set_timeout(zeit)
-		msg.show()
 
-''' Tried to workaround the close problem... '''
-#		time.sleep(1)
-#		msg.update(" "," "," ")
-#		msg.show()
-#		time.sleep(2)
-#		msg.clsose() 
+# Prints Notifications
+
+class Notify(object):
+
+    def showNotification(self, notification, zeit):
+
+    # ...#.....import time
+
+        pynotify.init('CallAlertWindow')
+        msg = pynotify.Notification('CallAlert', notification,
+                                    'notification-message-im')
+        msg.set_urgency(pynotify.URGENCY_CRITICAL)
+        msg.set_hint('x', gtk.gdk.screen_width() / 2.)
+        msg.set_hint('y', gtk.gdk.screen_height() / 2.)
+        msg.set_timeout(zeit)
+        msg.show()
+
+
+# ........time.sleep(1)
+# ........msg.update(" "," "," ")
+# ........msg.show()
+# ........time.sleep(2)
+# ........msg.clsose()
 
 class NcidClient(LineReceiver):
-	def lineReceived(self, line):
-		if line.startswith('200 NCID Server:  ARC_ncidd 0.01'):
-			print "Date:	   ","Time: ","Number:"
-		elif line.startswith('CIDLOG:'):
-			data = line.split('*')
-			date, time, phonenr = data[2], data[4], data[8]
-			tstamp_log = datetime.strptime('%s %s' % (date, time), '%d%m%Y %H%M').strftime('%Y-%m-%d %H:%M')
-			print ('%s %s' % (str(tstamp_log), phonenr))
-		elif line.startswith('CID: '):
-			calldata = line.split('*')
-			phonenr = calldata[8]
-			print ('You are called by ' + '%s' % str(phonenr))
-			notification = Notify()
-			notification.showNotification(('%s ' % str(phonenr)) + "calls you...",5000)
-		else:
-			print line
+
+    def lineReceived(self, line):
+        if line.startswith('200 NCID Server:  ARC_ncidd 0.01'):
+            print "Date:	   ", 'Time: ', 'Number:'
+        elif line.startswith('CIDLOG:'):
+            data = line.split('*')
+            (date, time, phonenr) = (data[2], data[4], data[8])
+            tstamp_log = datetime.strptime('%s %s' % (date, time),
+                    '%d%m%Y %H%M').strftime('%Y-%m-%d %H:%M')
+            print '%s %s' % (str(tstamp_log), phonenr)
+        elif line.startswith('CID: '):
+            calldata = line.split('*')
+            phonenr = calldata[8]
+            print 'You are called by ' + '%s' % str(phonenr)
+            notification = Notify()
+            notification.showNotification('%s ' % str(phonenr)
+                    + 'calls you...', 5000)
+        else:
+            print line
+
 
 class NcidClientFactory(ClientFactory):
-	protocol = NcidClient
 
-	def clientConnectionFailed(self, connector, reason):
-		print ('connection failed: %s' % reason.getErrorMessage())
-		notification = Notify()
-		notification.showNotification("Connection failed",1000)
-		reactor.stop()
+    protocol = NcidClient
 
-	def clientConnectionFailed(self, connector, reason):
-		print ('connection lost: %s' % reason.getErrorMessage())
-		notification = Notify()
-		notification.showNotification("Connection lost",1000)
-		reactor.stop()
+    def clientConnectionFailed(self, connector, reason):
+        print 'connection failed: %s' % reason.getErrorMessage()
+        notification = Notify()
+        notification.showNotification('Connection failed', 1000)
+        reactor.stop()
+
+    def clientConnectionFailed(self, connector, reason):
+        print 'connection lost: %s' % reason.getErrorMessage()
+        notification = Notify()
+        notification.showNotification('Connection lost', 1000)
+        reactor.stop()
+
 
 class MyDaemon(daemon):
-#   Overrides run function in daemon
-	def run(self):
-		print '[+] Initializing...'
-		notification = Notify()
-		notification.showNotification("Initializing",500) # Takes too long to close...
-		factory = NcidClientFactory()
-		reactor.connectTCP(SERVER_IP, SERVER_PORT, factory)
-		print '[+] Started!'
-#       notification.showNotification("Started",500) Takes too long to close...
-		reactor.run()   
-		print '[+] Stopped!'
+
+    #   Overrides run function in daemon
+
+    def run(self):
+        print '[+] Initializing...'
+        notification = Notify()
+        notification.showNotification('Initializing', 500)  # Takes too long to close...
+        factory = NcidClientFactory()
+        reactor.connectTCP(SERVER_IP, SERVER_PORT, factory)
+        print '[+] Started!'
+
+                    #       notification.showNotification("Started",500) Takes too long to close...
+
+        reactor.run()
+        print '[+] Stopped!'
+
+
 #       notification.showNotification("Stopped",500) Same here :D
 
 if __name__ == '__main__':
 
     daemon = MyDaemon('/tmp/CallAlert.pid')
     if len(sys.argv) == 2:
-                if 'start' == sys.argv[1]:
-                    daemon.start()
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+
 #                    if os.path.isfile('/tmp/CallAlert.pid'):
 #                        os.remove('/tmp/CallAlert.pid')
-                elif 'stop' == sys.argv[1]:
-                        daemon.stop()
-                elif 'restart' == sys.argv[1]:
-                        daemon.restart()
-                elif 'status' == sys.argv[1]:
-                        daemon.status()
-                else:
-                        print "Unknown command"
-                        sys.exit(2)
-                sys.exit(0)
+
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        elif 'status' == sys.argv[1]:
+            daemon.status()
+        else:
+            print 'Unknown command'
+            sys.exit(2)
+        sys.exit(0)
     else:
-                print "Usage: %s start|stop|restart|status" % sys.argv[0]
-                sys.exit(2)
+        print 'Usage: %s start|stop|restart|status' % sys.argv[0]
+        sys.exit(2)
+
+
